78bffc2d57adbb816191ba6b506be2e5946beab8
Soejun <wc.wilsonch@gmail.com>
2021-02-24 15:55:54 -0800

    Test computerPlay

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,5 +1,10 @@
 //rockPaperscissors
 
 function computerPlay(){
-    let compChoices = ['Rock', 'Paper', 'Scissors']
+    let compChoices = ['Rock', 'Paper', 'Scissors']; //Creates an array of rock, paper, scissor choices
+    let finChoice = Math.floor(Math.random() * compChoices.length);
+
+    return compChoices([random]);
 }
+
+console.log(computerPlay)
